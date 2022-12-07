e66c694a1fd60e69cbaae4a806bfeb15bd10c72f
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-12-06 23:31:42 -0300

    background image added. desktop modelling should be done after mobile first.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ export const Home = ()=>{
     return(
         <main className="flex items-center justify-center h-screen bg-sky-100">
             <div className=" w-3/4 h-5/6 flex items-center px-2 bg-white">
-                <aside className=" h-95 w-2/6 bg-blue-200 mx-3">
+                <aside className=" h-95 w-60 bg-blue-200 mx-3 sidebarBg">
                     <p>Hola Fer</p>
 
                 </aside>
