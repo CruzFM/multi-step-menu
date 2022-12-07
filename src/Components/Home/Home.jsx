95ead21bc519261a80cef57e3de0103b3fc7ee32
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-12-06 22:18:18 -0300

    aside modelling almost complete.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,12 @@
 export const Home = ()=>{
     return(
-        <main className="flex items-center justify-center h-screen">
-            <div className=" w-3/4 h-3/4 bg-slate-500 flex items-center">
-                <aside className=" h-5/6 w-2/6 bg-blue-200 ">
+        <main className="flex items-center justify-center h-screen bg-sky-100">
+            <div className=" w-3/4 h-5/6 flex items-center px-2 bg-white">
+                <aside className=" h-95 w-2/6 bg-blue-200 mx-3">
                     <p>Hola Fer</p>
 
                 </aside>
-                <article>
+                <article className=" h-5/6 w-8/12">
                     <p>Hola a todos.</p>
                 </article>
             </div>
