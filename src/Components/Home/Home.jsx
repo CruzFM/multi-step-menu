741f1f2a708c1db5da9b291632d19bf3d54fc52c
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-12-08 15:25:32 -0300

    Mobile modelling continued.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,8 +12,23 @@ export const Home = ()=>{
             </div> */}
 
             <div className=" h-full flex flex-col justify-center relative md:w-3/4 md:h-5/6 md:flex-row md:px-2 md:bg-white md:items-center">
-                <aside className=" w-screen h-1/4 sidebarBg-mov md:h-95 md:w-3/12 md:mx-3">
-                    <p>Hola Fer</p>
+                <aside className=" w-screen h-1/4 sidebarBg-mov md:h-95 md:w-3/12 md:mx-3 p-4">
+                    {/* md:py-4 md:px-2  */}
+                    {/* <p>Hola Fer</p> */}
+                    <ul className="flex gap-1 md:flex-col border border-solid border-black justify-around px-9">
+                        <li className="border border-solid border-white rounded-full px-2">
+                            1
+                        </li>
+                        <li className="border border-solid border-white rounded-full px-2">
+                            2
+                        </li>
+                        <li className="border border-solid border-white rounded-full px-2">
+                            3
+                        </li>
+                        <li className="border border-solid border-white rounded-full px-2">
+                            4
+                        </li>
+                    </ul>
                 </aside>
                 <div className=" h-3/4 flex-col border border-black border-solid md:w-9/12">
                     <div className=" h-4/5 bg-sky-100">
