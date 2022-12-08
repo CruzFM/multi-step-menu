3ddbd24e5f75918cd38eacc10d5f7a63cc156522
Fer Cruz <cruzfernandomartin94@gmail.com>
2022-12-08 14:57:20 -0300

    Desktop version started.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,6 @@
 export const Home = ()=>{
     return(
-        <main className="flex items-start justify-center h-screen bg-sky-100 relative">
+        <main className="flex items-start justify-center h-screen bg-sky-100 relative md:items-center">
             {/* <div className=" w-3/4 h-5/6 flex items-center px-2 bg-white">
                 <aside className=" h-95 w-60 bg-blue-200 mx-3 sidebarBg">
                     <p>Hola Fer</p>
@@ -11,13 +11,13 @@ export const Home = ()=>{
                 </article>
             </div> */}
 
-            <div className=" h-full flex-col items-center justify-center  relative">
-                <aside className=" w-screen h-1/4 sidebarBg-mov">
+            <div className=" h-full flex flex-col justify-center relative md:w-3/4 md:h-5/6 md:flex-row md:px-2 md:bg-white md:items-center">
+                <aside className=" w-screen h-1/4 sidebarBg-mov md:h-95 md:w-3/12 md:mx-3">
                     <p>Hola Fer</p>
                 </aside>
-                <div className=" h-3/4 flex-col border border-black border-solid">
+                <div className=" h-3/4 flex-col border border-black border-solid md:w-9/12">
                     <div className=" h-4/5 bg-sky-100">
-                        <article className=" w-11/12 bg-white  border border-solid border-black  absolute top-28 left-1 h-2/4">
+                        <article className=" w-11/12 bg-white  border border-solid border-black  absolute top-28 left-1 h-2/4 p-3 rounded-lg md:static">
                             {/* absolute top-24 left-1  */}
                             <p>Hola a todos.</p>
                         </article>
